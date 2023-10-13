43f0a159612d4d2372aa15ee7f99b257772309d3
Vladimir Privalov <vovaprivalov@gmail.com>
2023-10-13 17:10:49 +0400

    Refactor code

--- video_utils/record_video.py
+++ video_utils/record_video.py
@@ -1,11 +1,10 @@
-import numpy as np
 import argparse
-import cv2
 
+import cv2
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--file", required=True,
-    help="path to output video file")
+                help="path to output video file")
 
 
 def record_video():
@@ -18,15 +17,15 @@ def record_video():
     # Define the codec and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(args['file'],
-                          fourcc, 20.0, (640,480))
+                          fourcc, 20.0, (640, 480))
 
-    while(cap.isOpened()):
+    while (cap.isOpened()):
         ret, frame = cap.read()
-        if ret==True:
+        if ret == True:
             # write the flipped frame
             out.write(frame)
 
-            cv2.imshow('frame',frame)
+            cv2.imshow('frame', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
